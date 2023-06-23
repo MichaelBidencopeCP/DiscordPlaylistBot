a2d9ed197ce3ee6f1f5d348c7e813bb7f96ffce5
Mike <mbidencope3@gmail.com>
2023-06-23 12:17:21 -0400

    database commit

---
+++ database/databaseinator.py
@@ -0,0 +1,39 @@
+import sqlite3
+import datetime
+class Database_controlinator():
+    def __init__(self):
+        self.conn = sqlite3.connect('database/songs.db')
+        self.c = self.conn.cursor()
+    #insterts new song into database
+    def insert_song_into_db(self, song_name: str, song_link:str, first_played:datetime, played_by:int):
+        #make query to database to insert song
+        res = self.c.execute("INSERT INTO songs(song_name, song_link, first_played, played_by, skips) VALUES(?, ?, ?, ?, ?)", (song_name, song_link, first_played, played_by, 0))        
+        self.conn.commit()
+        print(res)
+        if res.rowcount == 1:
+            return True
+        else:
+            return False
+    
+    def add_song_skip(self, song_id:int):
+        res = self.c.execute("UPDATE songs SET skips = skips + 1 WHERE song_id = ?", (song_id,))
+        self.conn.commit()
+        if res.rowcount == 1:
+            return True
+        else:
+            return False
+    
+    def get_song_by_id(self, song_id:int):
+        res = self.c.execute("SELECT * FROM songs WHERE song_id = ?", (song_id,))
+        return res.fetchone()
+    
+    def check_if_song_link_exists(self, song_link:str):
+        res = self.c.execute("SELECT * FROM songs WHERE song_link = ?", (song_link,))
+        return res.fetchone()
+    
+    def get_all_songs(self):
+        res = self.c.execute("SELECT * FROM songs")
+        return res.fetchall()
+    
+test = Database_controlinator()
+print(test.get_all_songs())
